be176beffda750bd457e8ccb1e818ebab0e1d803
Jason Nuhn <jason.a.nuhn@gmail.com>
2018-04-06 12:13:14 -0400

    updated directory

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+
+import LandingPage from "./components/LandingPage";
+import Authorisation from "./components/Authorisation";
+import RequireAuth from "./components/RequireAuthHOC";
+import InvoiceList from "./components/InvoiceList";
+import InvoicePage from "./components/InvoicePage";
+import Billing from "./components/Billing";
+import Settings from "./components/Settings";
+import NotFound from "./components/NotFound";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/authorisation" component={Authorisation} />
+          <Route path="/invoices" component={RequireAuth(InvoiceList)} />
+          <Route path="/new" component={RequireAuth(InvoicePage)} />
+          <Route path="/billing" component={Billing} />
+          {/* <Route path="/billing" component={RequireAuth(Billing)} /> */}
+          <Route path="/settings" component={RequireAuth(Settings)} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     );
   }
 }
