2c0c684f3b13e141f373e199c34567082306465b
Jason Nuhn <jason.a.nuhn@gmail.com>
2018-04-12 17:27:53 -0400

    update build

--- src/components/Authorisation/SignUpForm.js
+++ src/components/Authorisation/SignUpForm.js
@@ -88,7 +88,7 @@ class SignUpForm extends Component {
     else this.setState({ confirmPasswordErr: "" });
 
     axios
-      .post(`https://lspaperstack.herokuapp.com/new-user`, { email, password })
+      .post(`https://localhost:3000/new-user`, { email, password })
       .then(res => {
         localStorage.setItem("tkn", res.data.token);
         this.props.history.push("/invoices");
